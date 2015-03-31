d260fb33cc5e4f55e683b38bf31eb52c8c7599ff
Rafael Leite <rafael.leite@controlid.com.br>
2015-03-31 18:34:54 -0300

    example of http request using golang

--- clientHandler.go
+++ clientHandler.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"text/template"
+
+	"github.com/rcmgleite/labEngSoft_Estoque/models"
 )
 
 //defaultHandler Just redirect the incomming default "/" request to index
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	var p models.Product
+	p.ID = 3
+	bJSON, err := json.Marshal(p)
+	req, err := http.NewRequest("DELETE", "http://127.0.0.1:8080/product", bytes.NewBuffer(bJSON))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
 	t, _ := template.ParseFiles("views/html/index.html")
 	t.Execute(w, nil)
 }
