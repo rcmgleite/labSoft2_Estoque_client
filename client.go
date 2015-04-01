03fd75d1f6d5c04772e70f88fc9f959141d41afc
Rafael Leite <rafael.leite@controlid.com.br>
2015-03-31 22:57:17 -0300

    two examples of how to make requests with go

--- client.go
+++ client.go
@@ -11,6 +11,9 @@ import (
 func main() {
 	r := router.NewRouter()
 	r.AddRoute("/", router.GET, defaultHandler)
+	r.AddRoute("/product", router.GET, GETProductHandler)
+	r.AddRoute("/product", router.DELETE, DELETEProductHandler)
+
 	http.Handle("/", r)
 
 	fmt.Println("Client running on port: 8081")
--- clientHandler.go
+++ clientHandler.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"text/template"
@@ -11,25 +12,74 @@ import (
 	"github.com/rcmgleite/labEngSoft_Estoque/models"
 )
 
-//defaultHandler Just redirect the incomming default "/" request to index
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	var p models.Product
-	p.ID = 3
-	bJSON, err := json.Marshal(p)
-	req, err := http.NewRequest("DELETE", "http://127.0.0.1:8080/product", bytes.NewBuffer(bJSON))
-	req.Header.Set("Content-Type", "application/json")
+func makeRequest(httpMethod string, url string, requestObj []byte, headers map[string]string) (*http.Response, error) {
+	req, err := http.NewRequest(httpMethod, url, bytes.NewBuffer(requestObj))
+	addHeaders(req, headers)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
+	return resp, nil
 
+}
+
+func parseJSON(decodedJSON io.ReadCloser, object interface{}) {
+	decoder := json.NewDecoder(decodedJSON)
+	decoder.Decode(object)
+}
+
+func getJSON(object interface{}) ([]byte, error) {
+	if object != nil {
+		return json.Marshal(object)
+	}
+	return nil, nil
+}
+
+func addHeaders(req *http.Request, headers map[string]string) {
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+}
+
+//defaultHandler Just redirect the incomming default "/" request to index
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("views/html/index.html")
 	t.Execute(w, nil)
+}
+
+// GETProductHandler ...
+func GETProductHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("views/html/product.html")
+	var products []models.Product
+
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+	response, err := makeRequest("GET", "http://127.0.0.1:8080/product", nil, headers)
+	if response != nil && err == nil {
+		parseJSON(response.Body, &products)
+
+		t.Execute(w, products)
+	}
+	t.Execute(w, nil)
+}
+
+// DELETEProductHandler ...
+func DELETEProductHandler(w http.ResponseWriter, r *http.Request) {
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/json"
+
+	var toDelete models.Product
+	parseJSON(r.Body, &toDelete)
+
+	bJSON, err := getJSON(toDelete)
+	response, err := makeRequest("DELETE", "http://127.0.0.1:8080/product", bJSON, headers)
+
+	if response != nil && err == nil {
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Println("response Body:", string(body))
+		//TODO redirect
+	}
+
 }
