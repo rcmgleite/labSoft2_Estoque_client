9a82952031913aca9b8ad5f88ec16b89b163a795
Rafael Leite <rafael.leite@controlid.com.br>
2015-04-08 15:21:37 -0300

    import requestHelper

--- clientHandler.go
+++ clientHandler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -11,29 +10,13 @@ import (
 	"text/template"
 
 	"github.com/rcmgleite/labSoft2_Estoque/models"
+	"github.com/rcmgleite/labSoft2_Estoque/requestHelper"
 )
 
 //responseJSON
 type responseJSON struct {
 	ResponseBody interface{}
 	Error        string
-}
-
-func makeRequest(httpMethod string, url string, requestObj []byte, headers map[string]string) (*http.Response, error) {
-	//creating request obj
-	req, err := http.NewRequest(httpMethod, url, bytes.NewBuffer(requestObj))
-
-	//Adding request headers
-	addHeaders(req, headers)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-
 }
 
 func parseJSON(encodedJSON io.ReadCloser, object interface{}) {
@@ -47,12 +30,6 @@ func getJSON(object interface{}) ([]byte, error) {
 		return json.Marshal(object)
 	}
 	return nil, nil
-}
-
-func addHeaders(req *http.Request, headers map[string]string) {
-	for k, v := range headers {
-		req.Header.Set(k, v)
-	}
 }
 
 //defaultHandler Just redirect the incomming default "/" request to index
@@ -69,7 +46,7 @@ func GETProductHandler(w http.ResponseWriter, r *http.Request) {
 
 	headers := make(map[string]string)
 	headers["Content-Type"] = "application/json"
-	response, err := makeRequest("GET", "http://127.0.0.1:8080/product", nil, headers)
+	response, err := requestHelper.MakeRequest("GET", "http://127.0.0.1:8080/product", nil, headers)
 	if response != nil && err == nil {
 		parseJSON(response.Body, &rj)
 		t.Execute(w, rj.ResponseBody)
@@ -91,7 +68,7 @@ func DELETEProductHandler(w http.ResponseWriter, r *http.Request) {
 	//get JSON Marshaled as []byte
 	bJSON, err := getJSON(toDelete)
 	if err == nil {
-		response, err := makeRequest("DELETE", "http://127.0.0.1:8080/product", bJSON, headers)
+		response, err := requestHelper.MakeRequest("DELETE", "http://127.0.0.1:8080/product", bJSON, headers)
 
 		if response != nil && err == nil {
 			body, _ := ioutil.ReadAll(response.Body)
@@ -116,7 +93,7 @@ func POSTProductHandler(w http.ResponseWriter, r *http.Request) {
 
 	bJSON, err := getJSON(newProduct)
 	if err == nil {
-		_, err := makeRequest("POST", "http://127.0.0.1:8080/product", bJSON, headers)
+		_, err := requestHelper.MakeRequest("POST", "http://127.0.0.1:8080/product", bJSON, headers)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -136,7 +113,7 @@ func GETProductUpdate(w http.ResponseWriter, r *http.Request) {
 
 	url += "ID=" + strconv.Itoa(id)
 	fmt.Println(url)
-	response, _ := makeRequest("GET", url, nil, headers)
+	response, _ := requestHelper.MakeRequest("GET", url, nil, headers)
 
 	var rj responseJSON
 	parseJSON(response.Body, &rj)
@@ -161,7 +138,7 @@ func POSTProductUpdate(w http.ResponseWriter, r *http.Request) {
 
 	bJSON, err := getJSON(toUpdate)
 	if err == nil {
-		_, err := makeRequest("PUT", "http://127.0.0.1:8080/product", bJSON, headers)
+		_, err := requestHelper.MakeRequest("PUT", "http://127.0.0.1:8080/product", bJSON, headers)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -177,7 +154,7 @@ func GETOrderHandler(w http.ResponseWriter, r *http.Request) {
 
 	headers := make(map[string]string)
 	headers["Content-Type"] = "application/json"
-	response, err := makeRequest("GET", "http://127.0.0.1:8080/order", nil, headers)
+	response, err := requestHelper.MakeRequest("GET", "http://127.0.0.1:8080/order", nil, headers)
 	if response != nil && err == nil {
 		parseJSON(response.Body, &rj)
 		t.Execute(w, rj.ResponseBody)
@@ -195,7 +172,7 @@ func POSTOrderHandler(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		headers := make(map[string]string)
 		headers["Content-Type"] = "application/json"
-		_, err := makeRequest("PUT", "http://127.0.0.1:8080/order", bJSON, headers)
+		_, err := requestHelper.MakeRequest("PUT", "http://127.0.0.1:8080/order", bJSON, headers)
 		if err != nil {
 			fmt.Println(err)
 		}
